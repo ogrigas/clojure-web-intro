2fa39f26bc0b16a674ac5b0d4bd9512828dd60c9
Osvaldas Grigas <osvaldas@inventi.lt>
2014-10-30 15:00:00 +0200

    Add movie list page

--- src/intro/controller.clj
+++ src/intro/controller.clj
@@ -3,3 +3,7 @@
             [hiccup.core :refer [html]]
             [stencil.core :refer [render-file]]
             [intro.database :as db]))
+
+(defn movie-list []
+  (render-file "templates/movie-list.html"
+               {:movies @db/movies}))
--- src/intro/routing.clj
+++ src/intro/routing.clj
@@ -6,5 +6,10 @@
             [ring.util.response :as resp]
             [intro.controller :as controller]))
 
-(defn handler [req]
-  {:status 200})
+(def handler
+  (routes
+    (GET "/" []
+         (resp/redirect "/movie-list"))
+    (GET "/movie-list" []
+         (controller/movie-list))
+    (route/not-found "Oops! Not Found")))
